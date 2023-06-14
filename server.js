b20fa90e3d3cee783046f639f7e5f38e44d53a96
haykqyutukyan <85229880+haykqyutukyan@users.noreply.github.com>
2023-06-14 21:19:58 +0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,86 +1,101 @@
-import express from "express";
-import path from "path";
+// import express from "express";
+// import { MongoClient } from "mongodb";
 
 
-const port = 3000; 
-const app = express();
+// const app = express();
+// const client = new MongoClient("mongodb://localhost:27017")
 
  
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-   res.sendFile(path.resolve("public/index.html"));
-});
+// app.use(express.static("public"));
+// app.use(express.json());
 
 
-let curTime = new Date();
-let day = curTime.getDate()
-let month = curTime.getMonth()+1
-let year = curTime.getFullYear()
-let toDos = [
-   {name:"learn C", deadLine: year + ".0" + month + "." + day, completed:false},
-   {name:"learn JS", deadLine: year + ".0" + month + "." + day, completed:false},
-   {name:"learn architecture", deadLine: year + ".0" + month + "." + day, completed:false}
-]
 
+// (async function(){
+//   await client.connect();
+//   const cleanup = (event) => {
+//     client.close();
+//     process.exit()
+//   }
+//   process.on("SIGINT", cleanup);
+//   process.on("SIGTERM", cleanup);
 
-app.get('/todos', (req, res) => {
-  res.send(toDos);
-});
+//   const db = client.db("TodoList")
+//   const collection = db.collection("toDos")
+//   await collection.insertOne(
+//     {
+//       toDos: [
+//         {name:"learn C", deadLine: year + ".0" + month + "." + day, completed:false},
+//         {name:"learn JS", deadLine: year + ".0" + month + "." + day, completed:false},
+//         {name:"learn architecture", deadLine: year + ".0" + month + "." + day, completed:false}
+//       ]
+//     }
+//   );
 
-// app.post('/todos', (req, res) => {
-//    toDos = req.body;
-//    res.send("Todos receieved")
+//   const findResult = await collection.find({}).toArray();
+//   console.log(findResult)
+  
+// })(); 
+
+// let curTime = new Date();
+// let day = curTime.getDate()
+// let month = curTime.getMonth()+1
+// let year = curTime.getFullYear()
+  
+// let toDos = [
+//    {name:"learn C", deadLine: year + ".0" + month + "." + day, completed:false},
+//    {name:"learn JS", deadLine: year + ".0" + month + "." + day, completed:false},
+//    {name:"learn architecture", deadLine: year + ".0" + month + "." + day, completed:false}
+// ]
+
+// app.get("/todos", (req, res) => {
+//   res.send(toDos);
 // });
 
+//   app.post("/todos", (req, res) => {
+//     toDos = req.body;
+//     res.send("Todos reseved")
+//   })
 
-app.post('/todos', (req, res) => {
-  const {name, deadLine, completed} = req.body;
-  const newTodo = { name, deadLine, completed };
-  toDos.push(newTodo);
-  res.status(201).json(newTodo);
-});
-
-
-app.put('/todos/:index', (req, res) => {
-  const index = req.params.index;
-  const { name, deadLine, completed } = req.body;
-  const todo = toDos[index];
-
-  if (!todo) {
-    res.status(404).json({ error: 'Task not found' });
-    return;
-  }
-
-  todo.name = name || todo.name;
-  todo.deadLine = deadLine || todo.deadLine;
-  todo.completed = completed !== undefined ? completed : todo.completed;
-
-  res.json(todo);
-});
+//   app.listen(3000)
 
 
-app.delete('/todos/:index', (req, res) => {
-  const index = req.params.index;
-  const todo = toDos[index];
+const express = require("express");
+const { MongoClient } = require("mongodb");
 
-  if (!todo) {
-    res.status(404).json({ error: 'Task not found' });
-    return;
-  }
+const app = express();
+const client = new MongoClient("mongodb://localhost:27017");
 
-  toDos.splice(index, 1);
-  res.sendStatus(204);
-});
+(async function () {
+  await client.connect();
+  const cleanup = (event) => {
+    client.close();
+    process.exit();
+  };
+  process.on("SIGINT", cleanup);
+  process.on("SIGTERM", cleanup);
+
+  const db = client.db("TodoList");
+  const collection = db.collection("toDos");
+
+  app.use(express.static("public"));
+  app.use(express.json());
+
+  app.get("/todos", async (req, res) => {
+    const dbTodos = await collection.findOne();
+    res.send(dbTodos.toDos);
+  });
+
+  app.post("/todos", async (req, res) => {
+    const newTodos = req.body;
+    await collection.updateOne({}, { $set: { toDos: newTodos } });
+    res.send("Todos received");
+  });
+
+  app.listen(3000, () => {
+    console.log("Server started on port 3000");
+  });
+})();
 
 
-app.get('/todos/stats', (req, res) => {
-  const completed = toDos.filter((todo) => todo.completed).length;
-  const pending = toDos.filter((todo) => !todo.completed).length;
-  res.json({ completed, pending });
-});
 
-
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
